1dc7216976fb8e8161110f47a14a002ee18ad87e
Yabe12 <yeabsirabehailu92@gmail.com>
2024-10-18 14:45:43 +0300

    correct the syntax of regiter student controller

--- student-registration-system/controllers/studentController.js
+++ student-registration-system/controllers/studentController.js
@@ -1,17 +1,28 @@
 const Student = require('../models/student');
 
-// Register a new student
+
 exports.registerStudent = async (req, res) => {
     const studentData = req.body;
 
     try {
+
+        const existingStudent = await Student.findOne({ email: studentData.email });
+        
+        if (existingStudent) {
+            return res.status(400).json({ message: 'Student already exists' });
+        }
+
+       
         const newStudent = new Student(studentData);
         await newStudent.save();
+        
         res.status(201).json({ message: 'Student registered successfully' });
     } catch (error) {
+        console.error(error); 
         res.status(500).json({ message: 'Server error' });
     }
 };
+
 
 // Approve a student
 exports.approveStudent = async (req, res) => {
